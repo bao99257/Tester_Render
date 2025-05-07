ad7a1c1644b0ee975ebc5b7fc0946f1aaddfac29
Bao99257@donga.edu.vn <YourGitHubEmail>
2025-05-08 02:32:14 +0700

    okee

--- start.sh
+++ start.sh
@@ -2,13 +2,11 @@
 
 # Chỉ chạy Admin và Customer services
 echo "Starting Admin service..."
-java -jar admin.jar --server.port=$PORT
-
+java -jar admin.jar --server.port=8087 --server.address=0.0.0.0 &
 ADMIN_PID=$!
 
 echo "Starting Customer service..."
-java -jar customer.jar --server.port=$PORT
-
+java -jar customer.jar --server.port=8020 --server.address=0.0.0.0 &
 CUSTOMER_PID=$!
 
 echo "All services started."
@@ -25,3 +23,4 @@ trap terminate SIGTERM SIGINT
 
 # Keep script running
 wait $ADMIN_PID $CUSTOMER_PID
+
